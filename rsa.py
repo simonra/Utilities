f5b8e05f4e39d39d336ea238d68b98230db59cf1
Simon Randby <simon.randby@gmail.com>
2018-07-03 19:24:43 +0200

    Make sure packaging and dissassembly actually works
    It is harder to split a string representing a number into correctly
    sized chunks to do math on than to do the same thing with a long
    binary string.
    
    Also make the tests more robust in the sense that the results no
    longer need to be manually checked.
    
    Next step now is reading e, d, and n form a standard rsa key file?

--- rsa.py
+++ rsa.py
@@ -1,50 +1,4 @@
-DEFAULT_BLOCK_SIZE = 4 # This value is in bytes. The block size has to be smaller than or equal to the key size, which is usually in bits.
-
-# def encrypt(message, product_of_initial_primes, public_key_exponent):
-# 	encrypted_message = 
-
-# def decrypt():
-
-# def check_if_prime():
-
-def split_text_into_chunks(long_text, block_size=DEFAULT_BLOCK_SIZE, pad_last_chunk=True):
-	# ecquivalent more compact, but perhaps less readable version:
-	# blocks = [long_text[i:i+block_size] for i in range(0, len(long_text), block_size)]
-	blocks = []
-	for i in range(0,len(long_text),block_size):
-		blocks.append(long_text[i:i+block_size])
-
-	if(pad_last_chunk):
-		if(len(blocks[-1])<block_size):
-			blocks[-1] = pad_text(blocks[-1],block_size)
-
-	return blocks
-
-def pad_text(text, desired_length, char_to_fill_with=' ', alignment='<'):
-	return '{message:{fill}{align}{width}}'.format(
-		message=text,
-		fill=char_to_fill_with,
-		align=alignment,
-		width=desired_length,
-	)
-
-def get_number_representing_text(text_as_string):
-	return int.from_bytes(text_as_string.encode('utf-8'), byteorder='big')
-
-def get_text_from_number(number_represention_text, block_size=DEFAULT_BLOCK_SIZE):
-	return int.to_bytes(number_represention_text, length=block_size, byteorder='big').decode('utf-8')
-
-def convert_list_of_texts_to_list_of_numbers(list_of_strings):
-	numbers = []
-	for text in list_of_strings:
-		numbers.append(get_number_representing_text(text))
-	return numbers
-
-def convert_list_of_numbers_to_list_of_texts(list_of_numbers, block_size=DEFAULT_BLOCK_SIZE):
-	texts = []
-	for number in list_of_numbers:
-		texts.append(get_text_from_number(number, block_size))
-	return texts
+DEFAULT_CHUNK_SIZE = 16 # This value is in bytes. The block size has to be smaller than or equal to the key size, which is usually in bits.
 
 def encrypt_list_of_numbers(list_of_numbers, e, n):
 	"""Args:
@@ -67,40 +21,29 @@ def decrypt_list_of_numbers(list_of_numbers, d, n):
 		decrypted_numbers.append(pow(number,d,n))
 	return decrypted_numbers
 
-def concatenate_numbers_to_string(list_of_numbers):
-	result = ""
-	for number in list_of_numbers:
-		result += str(number)
-	return result
-
-def concatenate_list_of_texts(list_of_texts):
-	result = ""
-	for text in list_of_texts:
-		result += text
-	return result
-
-def encrypt_message(message, e, n, block_size=DEFAULT_BLOCK_SIZE):
+def encrypt_message(message, e, n, chunk_size=DEFAULT_CHUNK_SIZE):
 	"""Args:
-		message (str): The plaintext message you want to encrypt.
+		message (b''): The binary message you want to encrypt.
 		e (int): The public key exponent you want to use to encrypt the message.
 		n (int): The public key modulus.
 	"""
-	chunked_message = split_text_into_chunks(message, block_size)
-	chunks_as_numbers = convert_list_of_texts_to_list_of_numbers(chunked_message)
+	chunked_message = chunk_array(message, chunk_size)
+	chunks_as_numbers = list_of_byte_chunks_to_list_of_numbers(chunked_message)
 	encrypted_chunks = encrypt_list_of_numbers(chunks_as_numbers, e, n)
-	encrypted_message = concatenate_numbers_to_string(encrypted_chunks)
+	encrypted_message = merge_byte_chunks(list_of_numbers_to_list_of_byte_chunks(encrypted_chunks))
 	return encrypted_message
 
-def decrypt_message(message, d, n, block_size=DEFAULT_BLOCK_SIZE):
+def decrypt_message(message, d, n, chunk_size=DEFAULT_CHUNK_SIZE):
 	"""Args:
 		message (int): The encrypted message you want to decrypt. Should be a number?
 		d (int): The private key exponent.
 		n (int): The private key modulus.
 	"""
-	chunked_message = split_text_into_chunks(message, block_size)
-	decrypted_chunks = decrypt_list_of_numbers(chunked_message)
-	chunks_as_strings = convert_list_of_numbers_to_list_of_texts(decrypted_chunks, block_size)
-	decrypted_message = concatenate_list_of_texts(chunks_as_strings)
+	chunked_message = chunk_array(message, chunk_size)
+	chunked_message_as_numbers = list_of_byte_chunks_to_list_of_numbers(chunked_message)
+	decrypted_chunks = decrypt_list_of_numbers(chunked_message_as_numbers)
+	chunks_as_binary_strings = list_of_numbers_to_list_of_byte_chunks(decrypted_chunks, block_size)
+	decrypted_message = merge_byte_chunks(chunks_as_strings)
 	return decrypted_message
 
 def read_byte_array_from_file(path_to_file):
@@ -140,19 +83,40 @@ def list_of_numbers_to_list_of_byte_chunks(list_of_numbers):
 	return byte_chunks
 
 if __name__ == "__main__":
-	text = 'My very long text.'
-	print(text)
-	print(len(text))
-	chunks = split_text_into_chunks(text)
-	print(chunks)
-	print(len(chunks[0]))
+	test_data = b'here is some random data\r\nI guess you could call it a binary blob?\r\nIt contains cool stuff such as "\xc3\xa6", \'\xc3\xb8\' and \\\xc3\xa5/!\r\n'
+	test_chunk_size = 4
+	if(test_data != number_to_bytes(bytes_to_number(test_data))):
+		print('Converting bytes to numbers and back again resulted in different bytes.')
+		print('The original bytes were:')
+		print(test_data)
+		print('The number obtained was:')
+		print(bytes_to_number(test_data))
+		print('When converting back, got these bytes:')
+		print(number_to_bytes(bytes_to_number(test_data)))
 
-	# reclaimedNumbers = get_text_from_number(88,33)
-	# print("reclaimed text = " + reclaimedNumbers)
+	if(test_data != merge_byte_chunks(chunk_array(test_data,test_chunk_size))):
+		print('Chunking bytes and stiching them together again failed.')
+		print('The original bytes were:')
+		print(test_data)
+		print('The chunked array was:')
+		print(chunk_array(test_data,test_chunk_size))
+		print('When merging the chunks, got these bytes:')
+		print(merge_byte_chunks(chunk_array(test_data,test_chunk_size)))
 
-	numbers = convert_list_of_texts_to_list_of_numbers(chunks)
-	print(numbers)
-	print((numbers[0].bit_length() + 7) // 8)
+	chunked_test_data = chunk_array(test_data,test_chunk_size)
+	if(chunked_test_data != list_of_numbers_to_list_of_byte_chunks(list_of_byte_chunks_to_list_of_numbers(chunked_test_data))):
+		print('Converting byte chunks to numbers and back again failed.')
+		print('The original byte chunks were:')
+		print(chunked_test_data)
+		print('The number array was:')
+		print(list_of_byte_chunks_to_list_of_numbers(chunked_test_data))
+		print('When converting back to bytes, got this list of bytes:')
+		print(list_of_numbers_to_list_of_byte_chunks(list_of_byte_chunks_to_list_of_numbers(chunked_test_data)))
 
-	texts = convert_list_of_numbers_to_list_of_texts(numbers)
-	print(texts)
+	number_chunks = list_of_byte_chunks_to_list_of_numbers(chunked_test_data)
+	represented_as_bytes = list_of_numbers_to_list_of_byte_chunks(number_chunks)
+	as_one_giant_byte_list = merge_byte_chunks(represented_as_bytes)
+	rechunked = chunk_array(as_one_giant_byte_list,test_chunk_size)
+	back_to_numbers = list_of_byte_chunks_to_list_of_numbers(rechunked)
+	if(number_chunks != back_to_numbers):
+		print('ToDo: Something is wrong. Express what so that it can be fixed')
