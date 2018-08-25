d565862bfd47e6fc8a72758e83fdcf1def855ceb
Simon Randby <simon.randby@gmail.com>
2018-08-25 14:28:39 +0200

    Chunk-file: some  restructuring
    Removes the auto-continue functionallity. It was a lot of messy code,
    and the operator in this case should checkt the state and specify how
    to handle it going on from there anyways.

--- chunk-file.py
+++ chunk-file.py
@@ -1,4 +1,3 @@
-import sys
 import argparse
 import os
 import math
@@ -32,13 +31,6 @@ assert os.path.exists(supplied_path_to_file), "Could not find file at " + suppli
 original_filename = os.path.basename(supplied_path_to_file)
 file_size_bytes = os.path.getsize(supplied_path_to_file)
 
-destination_directory = os.path.abspath(os.path.join(supplied_path_to_file, os.pardir))
-if(args.outputPath is not None):
-	destination_directory = args.outputPath
-	os.makedirs(destination_directory, exist_ok=True)
-
-# raise SystemExit
-
 # print('File size:')
 # print(file_size_bytes)
 # print('File name:')
@@ -46,7 +38,7 @@ if(args.outputPath is not None):
 
 # Smallest even number larger than 2^30 (1024^3) that is a multiple of 3
 block_size_bytes = 1073741826
-# block_size_bytes = 642414
+# block_size_bytes = 6424141
 
 # Round up, because we never want to miss that last block
 number_of_blocks = math.ceil(file_size_bytes / block_size_bytes)
@@ -59,43 +51,27 @@ if(number_of_blocks < 2):
 starting_block = 0
 if(args.startWith is not None):
 	# Don't care about what might have been before. What if the source is so lage we have to specify that this set of chunks need to go to a separate drive?
+	if(args.startWith > number_of_blocks):
+		print('Cannot start at chunk that is larger than total number of chunks. Aborting.')
+		print('Total number of block would be: ' + str(number_of_blocks))
+		print('Specified number of blocks: ' + str(args.startWith))
+		raise SystemExit
 	starting_block = args.startWith
-else:
-	# Figure out if we are simply continuing from where we left of
-	pre_existing_chunks = [f for f in os.listdir(destination_directory) if f.startswith(original_filename)]
-	# print(pre_existing_chunks)
-	if(args.outputPath is not None and len(pre_existing_chunks) == 1):
-		# Writing to same directory as source, and have to consider that the original file is not a chunk.
-		pass
-	elif(len(pre_existing_chunks) > 0):
-		# print("there are pre-existing chunks to check")
-		for named_chunk in pre_existing_chunks:
-			chunk_number = int(str.split(named_chunk,".")[-1])
-			# print('chunk_number')
-			# print(chunk_number)
-			if(chunk_number > starting_block):
-				starting_block = chunk_number
-		if(starting_block == number_of_blocks - 1):
-			if(len(pre_existing_chunks) >= number_of_blocks):
-				print('Larges part seems to be present in destination, and destination seems to contain all parts. Aborting.')
-				raise SystemExit
-			else:
-				print('Largest chunk seems to already be present. Starting over from scratch to be on the safe side.')
-				starting_block = 0
-		# else:
-			# Start not at last previously written block, but rather at the first missing block.
-			# Safer to not enable if you suspect that the last run failed during the writing of the last block.
-			# starting_block += 1
 
 # For when you know that you only want to extract a certain number of chunks at the moment.
 # Usefull for instance when trying to split a large file across several drives.
 if(args.numberOfBlocksToMake is not None):
 	number_of_blocks = args.numberOfBlocksToMake
 
+destination_directory = os.path.abspath(os.path.join(supplied_path_to_file, os.pardir))
+if(args.outputPath is not None):
+	destination_directory = args.outputPath
+	os.makedirs(destination_directory, exist_ok=True)
+
 # print('First block:')
 # print(starting_block)
 
-# for slow and messy connections where you want to have files open for as short as possible:
+# For slow and messy connections where you want to have files open for as short as possible:
 # for i in range(starting_block, number_of_blocks):
 # 	print('Starting to chunk ' + supplied_path_to_file + ' into parts in ' + destination_directory)
 # 	offset = starting_block * block_size_bytes
@@ -106,8 +82,7 @@ if(args.numberOfBlocksToMake is not None):
 
 # 	next_output_filename = original_filename + "." + str(i)
 # 	destination_path = os.path.join(destination_directory, next_output_filename)
-# 	# print('Writing to destination path:')
-# 	# print(destination_path)
+# 	# print('Writing to destination path: ' + destination_path)
 # 	# Uncomment in case you can't reliably write bytes to the destination:
 # 	# current_block = base64.b64encode(current_block)
 # 	with open(destination_path,'wb') as destination_file:
@@ -122,8 +97,7 @@ with open(supplied_path_to_file,'rb') as source_file:
 		current_block = source_file.read(block_size_bytes)
 		next_output_filename = original_filename + "." + str(i)
 		destination_path = os.path.join(destination_directory, next_output_filename)
-		# print('Writing to destination path:')
-		# print(destination_path)
+		# print('Writing to destination path: ' + destination_path)
 		# Uncomment in case you can't reliably write bytes to the destination:
 		# current_block = base64.b64encode(current_block)
 		with open(destination_path,'wb') as destination_file:
