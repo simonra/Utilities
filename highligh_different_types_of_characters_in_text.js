fbe157f094b9a9400593930f135ac32026a423e3
Simon Randby <simon.randby@gmail.com>
2021-12-05 23:04:37 +0100

    Add script for hightlighting what parts of strings are, numbers, letters, or other thing
    I just got a reference code with 3 lookalike characters. I decided
    that no, I do not want to figure out a clever way to ascertain what
    is what again, just make a script that neatly highlights everything
    for me.

---
+++ highligh_different_types_of_characters_in_text.js
@@ -0,0 +1,61 @@
+var textToClarify = "My? Or Your? Text. It has numbers, like 1, and 1000! OH~";
+
+// const BackgroundColour = '#fff' // white
+// const LetterColour = '#008080' // teal
+// const NumberColour = '#800080' // purple
+// const OtherColour = '#9a6510' // Golden-brown
+
+const BackgroundColour = '#2e2e2e'
+const LetterColour = '#b4d273'
+const NumberColour = '#6c99bb'
+const OtherColour = '#e87d3e'
+
+function IsCharacterDigit(character){
+	if(0 <= character && character <= 9){
+		return true;
+	}
+	return false;
+}
+
+function IsCharacterLetter(character){
+	// If the character has a different upper and lower case representation it is reasonable for my use to assume that it is a letter.
+	// It might even be a bit too inclusive, I guess if it annoys me over time I'll go for something more limmited like "return (/[a-zA-Z]/).test(char)"
+	return character.toLowerCase() != character.toUpperCase();
+}
+
+var ListOfCharactersWithProperties = [];
+var ListOfCssStrings = [];
+var ReassembledStringForPrinting = "";
+for(const character of textToClarify){
+	var colour = OtherColour;
+	if(IsCharacterLetter(character)){
+		colour = LetterColour;
+	}
+	else if(IsCharacterDigit(character)){
+		colour = NumberColour;
+	}
+
+	var CssString = `background: ${BackgroundColour} color: ${colour};`;
+	var PrintString = `%c${character}`;
+
+	var characterWithMetadata = {
+		Character: character,
+		StringRepresentationForPrinting: PrintString,
+		CssForPrinting: CssString
+	};
+
+	ListOfCharactersWithProperties.push(characterWithMetadata);
+	ListOfCssStrings.push(CssString);
+	ReassembledStringForPrinting += PrintString;
+
+	// console.log(character);
+}
+
+console.log(ReassembledStringForPrinting, ...ListOfCssStrings);
+
+
+
+
+// Use this for coloring:
+// https://stackoverflow.com/a/13017382
+// console.log("%cStr1" + "%cStr2", "CSS-for-str1", "CSS-for-str2")
