076c1234133450e1b9bf4f2a8aea4644e0cc6b67
Simon Randby <simon.randby@gmail.com>
2022-05-01 17:59:34 +0200

    Proton updaters: Improve output formatting

--- Proton_GE_install_scripts/install-steam-proton-ge.sh
+++ Proton_GE_install_scripts/install-steam-proton-ge.sh
@@ -9,7 +9,7 @@
 
 # Partially inspired by https://github.com/sirkhancision/update-proton-ge/
 
-echo "Installing latest version of GloriousEggroll Proton for Steam."
+echo ":: Installing latest version of GloriousEggroll Proton for Steam."
 
 # Constants
 URL_REPO=https://github.com/GloriousEggroll/proton-ge-custom
@@ -18,23 +18,23 @@ URL_LATEST_RELEASE=$URL_REPO/$LATEST_RELEASE_PATH
 PROTON_INSTALL_DIR=/home/$USER/.steam/root/compatibilitytools.d
 
 # Get latest version
-echo "Downloading name/tag of latest release."
+echo "  Downloading name/tag of latest release."
 url_latest_release_resolved_version=$(curl --silent --location --output /dev/null --write-out %{url_effective} --url $URL_LATEST_RELEASE)
 version=$(echo $url_latest_release_resolved_version | sed -E "s;^.*/;;")
-echo "Latest version is $version"
+echo "  Latest version is $version"
 
 install_dir=$PROTON_INSTALL_DIR/$version
 
-echo "Checking if already installed."
+echo "  Checking if already installed."
 if [ -d $install_dir ]
 then
-    echo "The latest version, $version, is alredy installed at $PROTON_INSTALL_DIR/."
-    echo "To re-install, delete the existing version and re-run the install latest script."
-    echo "To delete: rm -rf $install_dir"
+    echo "  The latest version, $version, is alredy installed at $PROTON_INSTALL_DIR/."
+    echo "    To re-install, delete the existing version and re-run the install latest script."
+    echo "    To delete: rm -rf $install_dir"
     exit
 fi
 
-echo "Downloading package and checksum."
+echo "  Downloading package and checksum."
 package_name=$version.tar.gz
 checksum_name=$version.sha512sum
 
@@ -51,14 +51,14 @@ checksum_file=/home/$USER/Downloads/$checksum_name
 curl --location --url $url_download_package --output $package_file
 curl --location --url $url_download_checksum --output $checksum_file
 
-echo "Download finished."
+echo "  Download finished."
 
 # BEGIN DEBUG
 # package_name=GE-Proton7-10.tar.gz
 # checksum_name=GE-Proton7-10.sha512sum
 # END DEBUG
 
-echo "Checking that package matches checksum."
+echo "  Checking that package matches checksum."
 
 # Find path to script and dir: https://stackoverflow.com/a/1638397/2890086
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
@@ -68,13 +68,13 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 if ! $($SCRIPTPATH/../verify-sha-checksum.sh --file $package_file --sum_file $checksum_file --quiet)
 then
-    echo "Error! Checksum verification failed. Hash of '$package_file' did not match value found in '$checksum_file'."
+    echo "  Error! Checksum verification failed. Hash of '$package_file' did not match value found in '$checksum_file'."
     exit 1
 fi
 
-echo "Extracting package to install directory."
+echo "  Extracting package to install directory."
 
 tar --extract --file $package_file --directory $PROTON_INSTALL_DIR
 
-echo "Done installing GloriousEggroll proton version $version for Steam. It should now be located at $install_dir. You have to restart Steam before the new version shows up and you can select it."
-echo "To restart Steam: pkill -TERM steam && sleep 5s && nohup steam </dev/null &>/dev/null &"
+echo "  Done installing GloriousEggroll proton version $version for Steam. It should now be located at $install_dir. You have to restart Steam before the new version shows up and you can select it."
+echo "  To restart Steam: pkill -TERM steam && sleep 5s && nohup steam </dev/null &>/dev/null &"
--- Proton_GE_install_scripts/install-wine-proton-ge.sh
+++ Proton_GE_install_scripts/install-wine-proton-ge.sh
@@ -13,7 +13,7 @@
 
 # tar --extract --file /home/$USER/Downloads/wine-lutris-GE-Proton7-10-x86_64.tar.xz --directory=/home/$USER/.local/share/lutris/runners/wine
 
-echo "Installing latest version of GloriousEggroll Proton for Wine for Lutris."
+echo ":: Installing latest version of GloriousEggroll Proton for Wine for Lutris."
 
 # Constants
 URL_REPO=https://github.com/GloriousEggroll/wine-ge-custom
@@ -22,23 +22,23 @@ URL_LATEST_RELEASE=$URL_REPO/$LATEST_RELEASE_PATH
 PROTON_INSTALL_DIR=/home/$USER/.local/share/lutris/runners/wine
 
 # Get latest version
-echo "Downloading name/tag of latest release."
+echo "  Downloading name/tag of latest release."
 url_latest_release_resolved_version=$(curl --silent --location --output /dev/null --write-out %{url_effective} --url $URL_LATEST_RELEASE)
 version=$(echo $url_latest_release_resolved_version | sed -E "s;^.*/;;")
-echo "Latest version is $version"
+echo "  Latest version is $version"
 
 install_dir=$PROTON_INSTALL_DIR/lutris-$version-x86_64
 
-echo "Checking if already installed."
+echo "  Checking if already installed."
 if [ -d $install_dir ]
 then
-    echo "The latest version, $version, is alredy installed at $PROTON_INSTALL_DIR/."
-    echo "To re-install, delete the existing version and re-run the install latest script."
-    echo "To delete: rm -rf $install_dir"
+    echo "  The latest version, $version, is alredy installed at $PROTON_INSTALL_DIR/."
+    echo "    To re-install, delete the existing version and re-run the install latest script."
+    echo "    To delete: rm -rf $install_dir"
     exit
 fi
 
-echo "Downloading package and checksum."
+echo "  Downloading package and checksum."
 package_name=wine-lutris-$version-x86_64.tar.xz
 checksum_name=wine-lutris-$version-x86_64.sha512sum
 
@@ -55,14 +55,14 @@ checksum_file=/home/$USER/Downloads/$checksum_name
 curl --location --url $url_download_package --output $package_file
 curl --location --url $url_download_checksum --output $checksum_file
 
-echo "Download finished."
+echo "  Download finished."
 
 # BEGIN DEBUG
 # package_name=GE-Proton7-10.tar.gz
 # checksum_name=GE-Proton7-10.sha512sum
 # END DEBUG
 
-echo "Checking that package matches checksum."
+echo "  Checking that package matches checksum."
 
 # Find path to script and dir: https://stackoverflow.com/a/1638397/2890086
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
@@ -76,8 +76,8 @@ then
     exit 1
 fi
 
-echo "Extracting package to install directory."
+echo "  Extracting package to install directory."
 
 tar --extract --file $package_file --directory $PROTON_INSTALL_DIR
 
-echo "Done installing GloriousEggroll proton version $version for Wine for Lutris. It should now be located at $install_dir. You have to restart Lutris before the new version shows up and you can select it."
+echo "  Done installing GloriousEggroll proton version $version for Wine for Lutris. It should now be located at $install_dir. You have to restart Lutris before the new version shows up and you can select it."
