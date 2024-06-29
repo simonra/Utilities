d1212e77bfce805ce640b9889532d42ccf2075c8
Simon Randby <simon.randby@gmail.com>
2024-06-29 15:31:10 +0200

    Kafka KvApi: Make sqlite password optional

--- Kafka/KeyValueApi/Code/KeyValueApi/KeyValueStateInSQLiteService.cs
+++ Kafka/KeyValueApi/Code/KeyValueApi/KeyValueStateInSQLiteService.cs
@@ -141,7 +141,12 @@ public class KeyValueStateInSQLiteService : IKeyValueStateService
         var configuredPassword = Environment.GetEnvironmentVariable(KV_API_STATE_STORAGE_SQLITE_PASSWORD);
         if(string.IsNullOrWhiteSpace(configuredPassword))
         {
-            throw new InvalidOperationException($"Env var {KV_API_STATE_STORAGE_SQLITE_PASSWORD} specifying SQLite password has to be set");
+            _logger.LogInformation($"Env var {KV_API_STATE_STORAGE_SQLITE_PASSWORD} specifying SQLite password has to be set, running without encrypting sqlite database on disk");
+            return new SqliteConnectionStringBuilder()
+            {
+                DataSource = location.FullName,
+                Mode = SqliteOpenMode.ReadWriteCreate
+            }.ToString();
         }
 
         var sqliteConnectionString = new SqliteConnectionStringBuilder()
